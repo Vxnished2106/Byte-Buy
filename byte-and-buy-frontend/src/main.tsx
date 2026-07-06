bcf7cb418a2ad7805bd1952b41a0b4299da1d633
Carlostala04 <carlostala.dev@gmail.com>
2026-07-05 22:10:29 -0600

    se encapsulo el app y peril para que sean accesibles solo si el usuario esta logeado

--- byte-and-buy-frontend/src/main.tsx
+++ byte-and-buy-frontend/src/main.tsx
@@ -7,15 +7,30 @@ import Register from "./pages/Register.tsx";
 import Login from "./pages/Login.tsx";
 import Perfil from "./pages/Perfil.tsx";
 import ForgotPassword from "./pages/ForgotPassword.tsx";
+import RequireAuth from "./components/RequireAuth.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route
+          path="/"
+          element={
+            <RequireAuth>
+              <App />
+            </RequireAuth>
+          }
+        />
         <Route path="/byte&buy/login" element={<Login />} />
         <Route path="/byte&buy/register" element={<Register />} />
-        <Route path="/byte&buy/perfil" element={<Perfil/>}/>
+        <Route
+          path="/byte&buy/perfil"
+          element={
+            <RequireAuth>
+              <Perfil />
+            </RequireAuth>
+          }
+        />
         <Route path="/byte&buy/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </BrowserRouter>
