b09ea84e798d2fc47b0224d7f075066cd76dac13
Carlostala04 <carlostala.dev@gmail.com>
2026-07-10 15:17:23 -0600

    se corrigio el dato de telefono de proveedor

--- byte-and-buy-frontend/src/ts/interfaces/proveedor.interface.ts
+++ byte-and-buy-frontend/src/ts/interfaces/proveedor.interface.ts
@@ -2,7 +2,7 @@ export interface proveedorData{
     proveedor_id: number
     proveedor_nombre:string
     proveedor_correo: string
-    proveedor_telefono:number
+    proveedor_telefono:string
     proveedor_direccion:string
     proveedor_estado:boolean
 }
