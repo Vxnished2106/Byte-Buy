7dcc7db9688aeebeabd4d4ae6bcf50427487ac11
Carlostala04 <carlostala.dev@gmail.com>
2026-07-10 15:17:09 -0600

    se creo la interfaz de producto

---
+++ byte-and-buy-frontend/src/ts/interfaces/producto.interface.ts
@@ -0,0 +1,11 @@
+export interface productoData {
+  producto_id: number;
+  producto_nombre: string;
+  producto_descripcion: string;
+  producto_categoria: string;
+  producto_descuento: number;
+  producto_impuesto: number;
+  producto_imagen: File | string; //Imagen o url de imagen existente
+  producto_banner: File | string; //Imagen o url de imagen existente
+  producto_estado: boolean;
+}
