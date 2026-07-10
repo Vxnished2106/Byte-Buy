b6a0754dc03b10efe0af3ebd76a334c4230475cc
Carlostala04 <carlostala.dev@gmail.com>
2026-07-10 15:16:57 -0600

    se agregaron las interfaces de proveedor y producto

--- byte-and-buy-frontend/src/ts/interfaces/index.ts
+++ byte-and-buy-frontend/src/ts/interfaces/index.ts
@@ -1,4 +1,6 @@
 export * from "./usuario.interface";
+export * from "./proveedor.interface"
+export * from "./producto.interface"
 export * from "./token-recuperacion.interface";
 export * from "./auth.interface";
 export * from "./recuperacion-contrasena.interface";
