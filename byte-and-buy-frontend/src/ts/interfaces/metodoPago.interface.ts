bdeb7334ce421e78cb55cf24463e173286ec30ac
Carlostala04 <carlostala.dev@gmail.com>
2026-07-18 17:10:12 -0600

    se crearon los interfaces de pago y metodo de pago

---
+++ byte-and-buy-frontend/src/ts/interfaces/metodoPago.interface.ts
@@ -0,0 +1,4 @@
+export interface metodoPago{
+    metodo_pago_id:number
+    metodo_pago_nombre:string
+}
---
+++ byte-and-buy-frontend/src/ts/interfaces/pago.interface.ts
@@ -0,0 +1,7 @@
+export interface Pago{
+    pago_id:number,
+    pago_monto:number,
+    pago_fecha:Date
+    pago_estado:string
+    pago_detalle:string
+}
