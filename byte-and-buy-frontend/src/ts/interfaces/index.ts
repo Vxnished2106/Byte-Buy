065e200335bb449c9a9fd15df4624f3614499ff4
Carlostala04 <carlostala.dev@gmail.com>
2026-07-18 17:10:24 -0600

    se cambio el import de los archivos

--- byte-and-buy-frontend/src/ts/interfaces/index.ts
+++ byte-and-buy-frontend/src/ts/interfaces/index.ts
@@ -1,12 +1,15 @@
 /** Barrel de todas las interfaces y tipos del dominio, para importar desde `@/ts/interfaces`. */
 export * from "./usuario.interface";
-export * from "./proveedor.interface"
-export * from "./categoria.interface"
-export * from "./etiqueta.interface"
-export * from "./producto.interface"
-export * from "./inventario.interface"
-export * from "./producto_proveedor.interface"
+export * from "./proveedor.interface";
+export * from "./categoria.interface";
+export * from "./etiqueta.interface";
+export * from "./producto.interface";
+export * from "./inventario.interface";
+export * from "./producto_proveedor.interface";
 export * from "./token-recuperacion.interface";
 export * from "./auth.interface";
-export * from "./recuperacion-contrasena.interface";export * from "./Carrito"
-export * from "./CarritoItem"
+export * from "./recuperacion-contrasena.interface";
+export * from "./carrito.interface";
+export * from "./carritoItem.interface";
+export * from "./pago.interface";
+export * from "./metodoPago.interface";
