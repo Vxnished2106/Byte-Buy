86e6714d564f1ed854caadbcb5372d2ebbfe9628
Carlostala04 <carlostala.dev@gmail.com>
2026-08-08 10:10:55 -0600

    se agregaron clases para el responsive

--- byte-and-buy-frontend/src/pages/Contacto.tsx
+++ byte-and-buy-frontend/src/pages/Contacto.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../components/Header";
 import "../styles/header.css";
+import "../styles/page-container.css";
 
 export default function Contacto() {
   return (
